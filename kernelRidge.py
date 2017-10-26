8a586c61b7230ce1c5f6b003c40c6979af89aa6d
fubao <freadream@gmail.com>
2017-10-26 13:03:01 -0400

    update_function

--- kernelRidge.py
+++ kernelRidge.py
@@ -6,6 +6,8 @@ Created on Tue Oct 24 11:30:43 2017
 @author: fubao
 """
 
+from sklearn.preprocessing import MinMaxScaler
+
 #implementation of kernel ridge regression
 import numpy as np
 
@@ -22,14 +24,16 @@ def KRRS(trainData, testData, i, lambdaPara):
     trainX = trainData[0]
     trainY = trainData[1]
     
-    
     testX = testData[0]
     testY = testData[1]
     
+    #trainX -= .5
+    #testX  -= .5
+    
+    minmax_scale
     print ("trainX shape[0]: ", trainX.shape[0], trainY.shape)
     
     #get alpha below
-
     kArr = np.empty((trainX.shape[0], trainX.shape[0]), dtype=np.float)
     print ("kArr shape original: ", kArr.shape)
 
@@ -53,7 +57,6 @@ def KRRS(trainData, testData, i, lambdaPara):
     ridgeParas = lambdaPara*np.identity(trainX.shape[0], dtype=np.float)
     
     alpha = np.dot(np.linalg.inv(kArr + ridgeParas), trainY)          #alpha for kernel ridge $\alpha = (\Phi(X)\phi^T(X)+\lambda I)^{-1}Y$ 
-    
     print ("ridgeParas: ", ridgeParas, alpha, alpha.shape)
     
     
@@ -62,8 +65,8 @@ def KRRS(trainData, testData, i, lambdaPara):
         xnew = testX[testInd]
         #for i in range(0, trainX.shape[0]):   # $y_{new} = \sum_{i}  \alpha_i \Phi(x_i) \Phi(x_{new}) 
         #innerVal = 
-        kij = np.sum([np.dot(alpha[i], (1 + np.dot(trainX[i].T, xnew))) for i in range(0, trainX.shape[0])])          #sum ??
+        ynew = np.sum([np.dot(alpha[i], (1 + np.dot(trainX[i], xnew))) for i in range(0, trainX.shape[0])])          #sum ??
 
         #alpha[i]
-        print ("xnew: ", xnew, kij)
+        print ("xnew: ", xnew, ynew)
         
